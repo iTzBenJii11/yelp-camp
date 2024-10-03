71cea11b1b24bb0ab52803f03c24914a16ece609
iTzBenJii <ben.wood@timesystemsuk.com>
2024-10-03 16:29:38 +0100

    remove api url due to work security policy inplace

--- randomJoke.js
+++ randomJoke.js
@@ -1,7 +1,7 @@
 // Random joke API
 module.exports.randomJoke = async () => {
   try {
-    let res = await fetch(`https://icanhazdadjoke.com/`, {
+    let res = await fetch(``, { // https://icanhazdadjoke.com/
       // Tells API we expect a JSON response
       headers: {
         Accept: "application/json",
@@ -13,3 +13,5 @@ module.exports.randomJoke = async () => {
     console.error(e);
   }
 };
+
+// Remove API at work due to security policy in place
